1ff6996d0ee0871b4c44a546594ccaccf38d09bc
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2015-09-12 12:04:30 +0300

    Splash Screen

---
+++ app/src/main/java/kiki__000/walkingstoursapp/SplashScreen.java
@@ -0,0 +1,98 @@
+package kiki__000.walkingstoursapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set the lang from sharedPreferences
+        MyApplication.updateLanguage(getApplicationContext());
+
+        setContentView(R.layout.activity_splash_screen);
+
+        //full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //textViews
+        TextView text1 = (TextView)findViewById(R.id.splash_text1);
+        TextView text2 = (TextView)findViewById(R.id.splash_text2);
+        TextView text3 = (TextView)findViewById(R.id.splash_text3);
+
+        // load animations
+        Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+
+        // Make fade in elements Visible first
+        text2.setVisibility(View.VISIBLE);
+
+        // start fade in animation
+        text2.startAnimation(fadeIn);
+        text3.startAnimation(fadeIn);
+
+        // start fade out animation
+        text1.startAnimation(fadeOut);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
